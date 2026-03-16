3f10664083243a43a16123e43d9d6e1c98866911
julian-CE <julian.pemberton05@gmail.com>
2026-03-15 21:23:40 -0400

    final changes

--- src/generatepage.py
+++ src/generatepage.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
             return line.strip()
     raise Exception("error: no h1 header")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     dest_dir = os.path.dirname(dest_path)
     os.makedirs(dest_dir, exist_ok=True)
@@ -22,14 +22,16 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(file_content)
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html)
+    template_content = template_content.replace("href=\"/", f"href=\"{basepath}")
+    template_content = template_content.replace("src=\"/", f"src=\"{basepath}")
     with open(dest_path, "w") as destination:
         destination.write(template_content)
 
-def generate_page_recursive(src, template_path, dest):
+def generate_page_recursive(src, template_path, dest, basepath):
     entries = os.listdir(src)
     for entry in entries:
         if os.path.isfile(os.path.join(src, entry)):
             dest_file = os.path.join(dest, entry.replace(".md", ".html"))
-            generate_page(os.path.join(src, entry), template_path, dest_file)
+            generate_page(os.path.join(src, entry), template_path, dest_file, basepath)
         else:
-            generate_page_recursive(os.path.join(src, entry), template_path, os.path.join(dest, entry))
+            generate_page_recursive(os.path.join(src, entry), template_path, os.path.join(dest, entry), basepath)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from copystatic import copy_static
 from generatepage import generate_page_recursive
+import sys
 
 def main():
+    basepath = sys.argv[1]
+    if not basepath:
+        basepath = "/"
     copy_static("static", "public")
-    generate_page_recursive("./content", "template.html", "./public")
+    generate_page_recursive("./content", "template.html", "./docs", basepath)
 
 main()
